92c0005d2d710308d089a9fd44de5c550b5d7255
yusvaaji <grafikyusvaaji@gmail.com>
2023-01-29 11:21:28 +0700

    test commit algoritam

--- basic-js/index.js
+++ basic-js/index.js
@@ -15,11 +15,14 @@ var tinggi = 5;
 
 var luas = 0.5 * alas * tinggi;
 testresut.innerHTML = `Luas Segitiga : ${luas} `;
+
+
+
 const testresultlingkaran = document.getElementById("testresultlingkaran");
 var r = 10;
 var phi = 22 / 7;
 
 var luaslingkaran = 2 * (phi * r);
-testresultlingkaran.innerHTML = `Luas Lingkaran : ${luaslingkaran} `;
+testresultlingkaran.innerHTML = `Jari jari : ${r} </br> Luas Lingkaran : ${luaslingkaran} `;
 
 //#endregion
