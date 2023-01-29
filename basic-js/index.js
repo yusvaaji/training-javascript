81c4c82cc9a53e6346bfdc5d5c8169eb98cd9ebd
yusvaaji <grafikyusvaaji@gmail.com>
2023-01-29 11:15:54 +0700

    test algoritma luas

--- basic-js/index.js
+++ basic-js/index.js
@@ -1,3 +1,25 @@
 //#region ES6
 
+//ALGORITMA DAN PSEUDOCODE
+
+/**
+ *
+ * 1. ALGORITMA
+ *
+ *
+ */
+
+const testresut = document.getElementById("testresult");
+var alas = 10;
+var tinggi = 5;
+
+var luas = 0.5 * alas * tinggi;
+testresut.innerHTML = `Luas Segitiga : ${luas} `;
+const testresultlingkaran = document.getElementById("testresultlingkaran");
+var r = 10;
+var phi = 22 / 7;
+
+var luaslingkaran = 2 * (phi * r);
+testresultlingkaran.innerHTML = `Luas Lingkaran : ${luaslingkaran} `;
+
 //#endregion
